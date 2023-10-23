242890150fd5a796ab0438f5b068fb58b7829ed6
ddddarcy <ddddarcy2222@163.com>
2023-10-23 23:53:07 +0800

    Testing for link protobuf lib and it's done.

--- demo3/test.cpp
+++ demo3/test.cpp
@@ -1,11 +1,15 @@
 #include<vector>
 #include<iostream>
-#include"demo3.h"
-
+//#include"demo3.h"
+#include"msg.pb.h"
 int main()
 {
-    Demo3 d3;
-    std::cout << "complete demo3 " << std::endl;
-    d3.start();
+    msg::MyMessage *message = new msg::MyMessage;
+    message->set_id(1);
+    message->set_name("Hello");
+    std::cout << message->name() << std::endl; 
+    // Demo3 d3;
+    // std::cout << "complete demo3 " << std::endl;
+    // d3.start();
     return 0;
 }
